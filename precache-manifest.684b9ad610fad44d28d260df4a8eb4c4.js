cd68d8880addbe556c9e93d602e5c0259b21a933
ParanoiaSystem <marcoferraioli@live.com>
2020-04-09 11:25:35 +0200

    Updates

---
+++ precache-manifest.684b9ad610fad44d28d260df4a8eb4c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a39250a2a0aaa6b9107875299053712",
+    "url": "/sospese/index.html"
+  },
+  {
+    "revision": "b851ed0055f2c24ac847",
+    "url": "/sospese/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4ed62edec1c94f59cd89",
+    "url": "/sospese/static/js/2.6bc024ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sospese/static/js/2.6bc024ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b851ed0055f2c24ac847",
+    "url": "/sospese/static/js/main.02cacadc.chunk.js"
+  },
+  {
+    "revision": "92bb41672e756860f0bf",
+    "url": "/sospese/static/js/runtime-main.87342b2f.js"
+  }
+]);
