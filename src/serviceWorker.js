e520faede8f6914ea2476c8c986fa6f79bfce8c4
ParanoiaSystem <marcoferraioli@live.com>
2020-04-09 15:32:16 +0200

    update serviceWorker and manifest

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -12,12 +12,12 @@
 
 const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
-    // [::1] is the IPv6 localhost address.
-    window.location.hostname === '[::1]' ||
-    // 127.0.0.0/8 are considered localhost for IPv4.
-    window.location.hostname.match(
-      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-    )
+  // [::1] is the IPv6 localhost address.
+  window.location.hostname === '[::1]' ||
+  // 127.0.0.1/8 is considered localhost for IPv4.
+  window.location.hostname.match(
+    /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+  )
 )
 
 export function register (config) {
@@ -43,7 +43,7 @@ export function register (config) {
         navigator.serviceWorker.ready.then(() => {
           console.log(
             'This web app is being served cache-first by a service ' +
-              'worker. To learn more, visit https://bit.ly/CRA-PWA'
+            'worker. To learn more, visit https://bit.ly/CRA-PWA'
           )
         })
       } else {
@@ -71,9 +71,9 @@ function registerValidSW (swUrl, config) {
               // content until all client tabs are closed.
               console.log(
                 'New content is available and will be used when all ' +
-                  'tabs for this page are closed. See https://bit.ly/CRA-PWA.'
+                'tabs for this page are closed. See https://bit.ly/CRA-PWA.'
               )
-
+              displayUpdateNotification()
               // Execute callback
               if (config && config.onUpdate) {
                 config.onUpdate(registration)
@@ -100,9 +100,7 @@ function registerValidSW (swUrl, config) {
 
 function checkValidServiceWorker (swUrl, config) {
   // Check if the service worker can be found. If it can't reload the page.
-  fetch(swUrl, {
-    headers: { 'Service-Worker': 'script' }
-  })
+  fetch(swUrl)
     .then(response => {
       // Ensure service worker exists, and that we really are getting a JS file.
       const contentType = response.headers.get('content-type')
@@ -128,14 +126,24 @@ function checkValidServiceWorker (swUrl, config) {
     })
 }
 
+function displayUpdateNotification () {
+  const link = document.createElement('a')
+  link.classList.add('update-notification')
+  link.setAttribute('href', '#')
+  link.innerHTML = 'Update is available. Click here to install.'
+
+  link.addEventListener('click', (e) => {
+    e.preventDefault()
+    window.location.reload()
+  })
+
+  document.querySelector('body').appendChild(link)
+}
+
 export function unregister () {
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.ready
-      .then(registration => {
-        registration.unregister()
-      })
-      .catch(error => {
-        console.error(error.message)
-      })
+    navigator.serviceWorker.ready.then(registration => {
+      registration.unregister()
+    })
   }
 }
