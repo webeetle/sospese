c0ae935bf4ab2eb4830f8ca9df940069f2018692
webeetle <info@webeetle.com>
2020-04-18 17:50:52 +0000

    Updates

--- precache-manifest.eb961980b37bc81fb845eacf4c8b547b.js
+++ precache-manifest.eb961980b37bc81fb845eacf4c8b547b.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "21ea4372400a08ad6d2a1ab0c95bfc5a",
+    "revision": "4c60ab552120bfa8ed28985d98e32f1e",
     "url": "/index.html"
   },
   {
-    "revision": "6fe3ffeadb7e7ce903aa",
-    "url": "/static/js/2.9087dee7.chunk.js"
+    "revision": "6a98023f8646dc801391",
+    "url": "/static/js/2.7cbf899e.chunk.js"
   },
   {
     "revision": "5e79975ab4e1760dfec791082f238687",
-    "url": "/static/js/2.9087dee7.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.7cbf899e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "270bb7a7689f8c761819",
-    "url": "/static/js/main.90f9d4aa.chunk.js"
+    "revision": "0c3867e23d1de91371f8",
+    "url": "/static/js/main.ae5a897f.chunk.js"
   },
   {
     "revision": "600ef500dff1d873a48f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9c9af38847a3ca835f4a735a47a3c88a.js"
+  "/precache-manifest.eb961980b37bc81fb845eacf4c8b547b.js"
 );
 
 self.addEventListener('message', (event) => {
